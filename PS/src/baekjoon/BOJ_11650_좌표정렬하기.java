3af1456764d11470e708bfa74d12904a43e38bd1
ssafypark <pbsu1213@hanmail.net>
2022-12-24 04:34:18 +0900

    feat: 람다 이용해서 정렬

--- PS/src/baekjoon/BOJ_11650_좌표정렬하기.java
+++ PS/src/baekjoon/BOJ_11650_좌표정렬하기.java
@@ -19,14 +19,11 @@ public class BOJ_11650_좌표정렬하기 {
             arr[i][1] = sc.nextInt();
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
-                    return o1[1] - o2[1];
-                } else {
-                    return o1[0] - o2[0];
-                }
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
             }
         });
 
