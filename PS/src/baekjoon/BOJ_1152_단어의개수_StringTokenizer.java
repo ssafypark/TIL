73d0b877fca0ab5a9d71cd20b903e49c8ea0e6a2
ssafypark <pbsu1213@hanmail.net>
2022-12-25 04:12:29 +0900

    feat: BOJ_1152_단어의개수 - StringTokenizer를 사용한 풀이

---
+++ PS/src/baekjoon/BOJ_1152_단어의개수_StringTokenizer.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BOJ_1152_단어의개수_StringTokenizer {
+
+    private static String str;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.println(st.countTokens());
+    }
+}
