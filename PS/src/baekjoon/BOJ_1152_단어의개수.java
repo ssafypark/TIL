da763f98a2bf6aad1008157fcfb8fae5abeb883d
ssafypark <pbsu1213@hanmail.net>
2022-12-25 04:11:57 +0900

    feat: BOJ_1152_단어의개수 - charAt을 사용한 풀이

---
+++ PS/src/baekjoon/BOJ_1152_단어의개수.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_1152_단어의개수 {
+
+    private static String str;
+    private static int cnt;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        cnt = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                cnt++;
+            }
+        }
+
+        if (str.charAt(0) == ' ' && str.charAt(str.length() - 1) == ' ') {
+            System.out.println(cnt - 1);
+        } else if (str.charAt(0) == ' ' || str.charAt(str.length() - 1) == ' ') {
+            System.out.println(cnt);
+        } else {
+            System.out.println(cnt + 1);
+        }
+    }
+}
