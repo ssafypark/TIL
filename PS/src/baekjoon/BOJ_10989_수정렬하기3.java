c38794cdf525e701b8608b0a6ff7dff24fbf5e61
ssafypark <pbsu1213@hanmail.net>
2022-12-26 21:20:23 +0900

    feat: BOJ_10989_수정렬하기3 - 스캐너를 사용하면 시간초과, 버퍼드리더와 스트링 빌더 사용

---
+++ PS/src/baekjoon/BOJ_10989_수정렬하기3.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_10989_수정렬하기3 {
+
+    private static int N;
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
