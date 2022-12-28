c32afcf9846a152d11cb515a5e8f999f1f556e71
ssafypark <pbsu1213@hanmail.net>
2022-12-28 20:54:06 +0900

    feat: SWEA_1966_숫자를정렬하자_D2 - 정렬문제

---
+++ PS/src/swea/SWEA_1966_숫자를정렬하자_D2.java
@@ -0,0 +1,32 @@
+package swea;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SWEA_1966_숫자를정렬하자_D2 {
+
+    private static int T;
+    private static int N;
+    private static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        T = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            N = sc.nextInt();
+            arr = new int[N];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+
+            System.out.print("#" + tc + " ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
