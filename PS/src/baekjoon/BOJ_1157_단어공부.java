bc6b53892dd152210422dc6b49cc71701a3f03ac
ssafypark <pbsu1213@hanmail.net>
2022-12-28 19:23:57 +0900

    feat: BOJ_1157_단어공부 - 아스키 코드 사용

---
+++ PS/src/baekjoon/BOJ_1157_단어공부.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1157_단어공부 {
+
+    private static int[] arr;
+    private static String str;
+    private static int max;
+    private static char ch;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        arr = new int[26];
+        str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') {
+                arr[str.charAt(i) - 97]++;
+            } else {
+                arr[str.charAt(i) - 65]++;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65);
+            } else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
+    }
+}
