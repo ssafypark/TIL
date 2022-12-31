de1031d5b54688e2c94bb15b7ef38ef7d635d425
ssafypark <pbsu1213@hanmail.net>
2023-01-01 03:48:31 +0900

    feat: BOJ_2908_상수 - StringBuilder를 이용한 풀이

---
+++ PS/src/baekjoon/BOJ_2908_상수.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2908_상수 {
+
+    private static int A;
+    private static int B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        System.out.println(A > B ? A : B);
+    }
+}
