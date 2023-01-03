c0eea10292baf322eef0c014d979a6fa5d0397bd
ssafypark <pbsu1213@hanmail.net>
2023-01-03 18:45:10 +0900

    feat: BOJ_5622_다이얼 - switch - case문을 이용한 풀이(컨벤션 수정)

--- PS/src/baekjoon/BOJ_5622_다이얼.java
+++ PS/src/baekjoon/BOJ_5622_다이얼.java
@@ -16,28 +16,28 @@ public class BOJ_5622_다이얼 {
         for (int i = 0; i < str.length(); i++) {
 
             switch (str.charAt(i)) {
-                case 'A' : case 'B' : case 'C' :
+                case 'A': case 'B': case 'C':
                     time += 3;
                     break;
-                case 'D' : case 'E' : case 'F' :
+                case 'D': case 'E': case 'F':
                     time += 4;
                     break;
-                case 'G' : case 'H' : case 'I' :
+                case 'G': case 'H': case 'I':
                     time += 5;
                     break;
-                case 'J' : case 'K' : case 'L' :
+                case 'J': case 'K': case 'L':
                     time += 6;
                     break;
-                case 'M' : case 'N' : case 'O' :
+                case 'M': case 'N': case 'O':
                     time += 7;
                     break;
-                case 'P' : case 'Q' : case 'R' : case 'S' :
+                case 'P': case 'Q': case 'R': case 'S':
                     time += 8;
                     break;
-                case 'T' : case 'U' : case 'V' :
+                case 'T': case 'U': case 'V':
                     time += 9;
                     break;
-                case 'W' : case 'X' : case 'Y' : case 'Z' :
+                case 'W': case 'X': case 'Y': case 'Z':
                     time += 10;
                     break;
             }
