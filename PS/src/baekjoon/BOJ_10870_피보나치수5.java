b23261f05ff101c2f2ff1950e3e56ad67b00f0dd
ssafypark <pbsu1213@hanmail.net>
2023-01-03 18:06:28 +0900

    feat: BOJ_10870_피보나치수5 - 피보나치 재귀 함수를 만들어서 풀이

---
+++ PS/src/baekjoon/BOJ_10870_피보나치수5.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10870_피보나치수5 {
+
+    private static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+
+        System.out.println(fibonacci(n));
+    }
+
+    private static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
