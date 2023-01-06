bde821e3da8727e3b631828cbe64525fa4f0e948
ssafypark <pbsu1213@hanmail.net>
2023-01-06 18:23:37 +0900

    feat: BOJ_10974_모든순열 - 방문처리를 이용한 순열 풀이(BufferedReader)

--- PS/src/baekjoon/silver/BOJ_10974_모든순열.java
+++ PS/src/baekjoon/silver/BOJ_10974_모든순열.java
@@ -1,5 +1,8 @@
 package baekjoon.silver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class BOJ_10974_모든순열 {
@@ -9,9 +12,9 @@ public class BOJ_10974_모든순열 {
     private static int[] output;
     private static boolean[] visited;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
         arr = new int[N];
         output = new int[N];
         visited = new boolean[N];
@@ -22,11 +25,12 @@ public class BOJ_10974_모든순열 {
     }
 
     private static void permutation(int depth) {
+        StringBuilder sb = new StringBuilder();
         if (depth == N) {
             for (int i = 0; i < N; i++) {
-                System.out.print(output[i] + " ");
+                sb.append(output[i]).append(" ");
             }
-            System.out.println();
+            System.out.println(sb);
         }
         for (int i = 0; i < N; i++) {
             if (!visited[i]) {
