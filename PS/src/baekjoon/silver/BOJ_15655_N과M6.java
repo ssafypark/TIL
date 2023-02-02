3f90cd1edc2bfa6e392f0d4cd88bfb5ab247f125
ssafypark <pbsu1213@hanmail.net>
2023-02-03 03:01:36 +0900

    feat: BOJ_15655_N과M6 - 정렬과 조합을 이용한 풀이

---
+++ PS/src/baekjoon/silver/BOJ_15655_N과M6.java
@@ -0,0 +1,49 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15655_N과M6 {
+
+    private static int N;
+    private static int M;
+    private static int[] arr;
+    private static int[] output;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        output = new int[M];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        combination(0, 0);
+    }
+
+    private static void combination(int depth, int idx) {
+        StringBuilder sb = new StringBuilder();
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            System.out.println(sb);
+            return;
+        }
+        for (int i = idx; i < N; i++) {
+            if (!visited[i]) {
+                output[depth] = arr[i];
+                combination(depth + 1, i + 1);
+            }
+        }
+    }
+}
