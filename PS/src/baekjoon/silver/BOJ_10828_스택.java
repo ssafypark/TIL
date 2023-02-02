ec12a43f9c14339669691ce37e0deb21dc94d608
ssafypark <pbsu1213@hanmail.net>
2023-02-03 01:37:52 +0900

    feat: BOJ_10828_스택 - 스택과 스위치 케이스 문을 이용한 풀이

---
+++ PS/src/baekjoon/silver/BOJ_10828_스택.java
@@ -0,0 +1,51 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_10828_스택 {
+
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push" :
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop" :
+                    if (stack.size() == 0) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size" :
+                    System.out.println(stack.size());
+                    break;
+                case "empty" :
+                    if (stack.size() == 0) {
+                        System.out.println(1);
+                    } else {
+                        stack.empty();
+                        System.out.println(0);
+                    }
+                    break;
+                case "top" :
+                    if (stack.size() == 0) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+            }
+        }
+    }
+}
