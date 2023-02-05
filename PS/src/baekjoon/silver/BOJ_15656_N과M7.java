ae7e44dc5a608618f236a3b013c8f8960a199cf3
ssafypark <pbsu1213@hanmail.net>
2023-02-05 20:54:33 +0900

    feat: BOJ_15656_N과M7 - 순열을 통한 풀이

---
+++ PS/src/baekjoon/silver/BOJ_15656_N과M7.java
@@ -0,0 +1,46 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15656_N과M7 {
+
+    private static int N;
+    private static int M;
+    private static int[] arr;
+    private static int[] output;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        output = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        permutation(0);
+        System.out.println(sb);
+    }
+
+    private static void permutation(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            output[depth] = arr[i];
+            permutation(depth + 1);
+        }
+    }
+}
