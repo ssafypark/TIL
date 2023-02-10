607c060f7dcca2c651aee2e689ac16095555f5a4
ssafypark <pbsu1213@hanmail.net>
2023-02-10 18:55:27 +0900

    feat: BOJ_2023_신기한소수 - DFS와 소수를 이용한 풀이

---
+++ PS/src/baekjoon/gold/BOJ_2023_신기한소수.java
@@ -0,0 +1,45 @@
+package baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2023_신기한소수 {
+
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        DFS(2, 1);
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
+    }
+
+    private static void DFS(int number, int depth) {
+        if (depth == N) {
+            if (isPrime(number)) {
+                System.out.println(number);
+            }
+            return;
+        }
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (isPrime(number * 10 + i)) {
+                DFS(number * 10 + i, depth + 1);
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
