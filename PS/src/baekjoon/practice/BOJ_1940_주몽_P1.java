eaacba6d5018f7d8182ec14c3afb6546e55d23a0
ssafypark <pbsu1213@hanmail.net>
2023-02-15 03:13:13 +0900

    feat: BOJ_1940_주몽_P1

---
+++ PS/src/baekjoon/practice/BOJ_1940_주몽_P1.java
@@ -0,0 +1,42 @@
+package baekjoon.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1940_주몽_P1 {
+
+    private static int N;
+    private static int M;
+    private static int startIndex;
+    private static int endIndex;
+    private static int cnt;
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        endIndex = N - 1;
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        while (startIndex < endIndex) {
+            if (arr[startIndex] + arr[endIndex] < M) {
+                startIndex++;
+            } else if (arr[startIndex] + arr[endIndex] == M) {
+                cnt++;
+                startIndex++;
+                endIndex--;
+            } else {
+                endIndex--;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
