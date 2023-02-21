ca15a11eecb761a90bc687489f25ec1e3134bf3d
ssafypark <pbsu1213@hanmail.net>
2023-02-21 17:16:07 +0900

    feat : BOJ_11659_구간합구하기4_P1 - 구간합

---
+++ PS/src/baekjoon/practice/BOJ_11659_구간합구하기4_P1.java
@@ -0,0 +1,37 @@
+package baekjoon.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11659_구간합구하기4_P1 {
+
+    private static int N;
+    private static int M;
+    private static int start;
+    private static int end;
+    private static int sum;
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            start = Integer.parseInt(st.nextToken());
+            end = Integer.parseInt(st.nextToken()) ;
+            sum = arr[end] - arr[start - 1];
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
