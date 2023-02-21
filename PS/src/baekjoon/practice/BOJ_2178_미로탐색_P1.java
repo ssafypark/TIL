ada989ac27ec6cb9ef0b2d46dbfb8b758353cf89
ssafypark <pbsu1213@hanmail.net>
2023-02-21 17:07:46 +0900

    feat : BOJ_2178_미로탐색_P1 - BFS를 통한 풀이

---
+++ PS/src/baekjoon/practice/BOJ_2178_미로탐색_P1.java
@@ -0,0 +1,56 @@
+package baekjoon.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_2178_미로탐색_P1 {
+
+    private static int N;
+    private static int M;
+    private static String str;
+    private static int[][] arr;
+    private static boolean[][] visited;
+    private static int[] dx = {0, 0, -1, 1};
+    private static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(str.substring(j, j + 1));
+            }
+        }
+        BFS(0, 0);
+        System.out.println(arr[N - 1][M - 1]);
+    }
+
+    private static void BFS(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {i, j});
+        visited[i][j] = true;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            for (int a = 0; a < 4; a++) {
+                int x = now[0] + dx[a];
+                int y = now[1] + dy[a];
+                if (x >= 0 && y >= 0 && x < N && y < M) {
+                    if (arr[x][y] != 0 && !visited[x][y]) {
+                        visited[x][y] = true;
+                        arr[x][y] = arr[now[0]][now[1]] + 1;
+                        queue.add(new int[] {x, y});
+                    }
+                }
+            }
+        }
+    }
+}
