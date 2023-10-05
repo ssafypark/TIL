202c7cc5dbae079b980cc231e7c9e33a3a4dce00
ssafypark <pbsu1213@hanmail.net>
2023-10-05 21:56:32 +0900

    feat: BOJ_3009_네번째점 - Map으로 풀이

---
+++ PS/src/baekjoon/bronze/BOJ_3009_네번째점_Map.java
@@ -0,0 +1,40 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ_3009_네번째점_Map {
+
+    private static int key;
+    private static Map<Integer, Integer> arrX;
+    private static Map<Integer, Integer> arrY;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        arrX = new HashMap<>();
+        arrY = new HashMap<>();
+        for (int i = 0 ; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            key = Integer.parseInt(st.nextToken());
+            arrX.put(key, arrX.getOrDefault(key, 0) + 1);
+            key = Integer.parseInt(st.nextToken());
+            arrY.put(key, arrY.getOrDefault(key, 0) + 1);
+        }
+        for (int keyX : arrX.keySet()) {
+            int value = arrX.get(keyX);
+            if (value == 1) {
+                System.out.print(keyX + " ");
+            }
+        }
+        for (int keyY : arrY.keySet()) {
+            int value = arrY.get(keyY);
+            if (value == 1) {
+                System.out.print(keyY);
+            }
+        }
+    }
+}
