f7087e9a6c3d2dd92c8c0cf73227783057882629
ssafypark <pbsu1213@hanmail.net>
2023-11-20 02:54:12 +0900

    feat : BOJ_1919_애너그램만들기 - 구현, 문자열

---
+++ PS/src/baekjoon/bronze/BOJ_1919_애너그램만들기.java
@@ -0,0 +1,34 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1919_애너그램만들기 {
+
+    private static String a;
+    private static String b;
+    private static int ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        a = br.readLine();
+        b = br.readLine();
+        int[] cntA = new int[26];
+        int[] cntB = new int[26];
+        for (int i = 0; i < a.length(); i++) {
+            cntA[a.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < b.length(); i++) {
+            cntB[b.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (cntA[i] > cntB[i]) {
+                ans += cntA[i] - cntB[i];
+            } else if (cntB[i] > cntA[i]) {
+                ans += cntB[i] - cntA[i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
