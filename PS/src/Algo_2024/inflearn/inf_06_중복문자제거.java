a1a099d17926e574b7c8c57aeb164c8e1aeac554
Park <pbsu1213@hanmail.net>
2024-04-27 19:29:08 +0900

    feature: str.indexOf로 중복 문자 비교

---
+++ PS/src/Algo_2024/inflearn/inf_06_중복문자제거.java
@@ -0,0 +1,24 @@
+package Algo_2024.inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_06_중복문자제거 {
+    public static String solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(solution(str));
+    }
+}
