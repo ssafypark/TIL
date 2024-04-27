ce39469864bec63b2d47ce4150c5a7dd257cca8d
Park <pbsu1213@hanmail.net>
2024-04-27 17:47:07 +0900

    feature: Ascii 코드로 풀이

--- PS/src/Algo_2024/inflearn/inf_02_대소문자변환.java
+++ PS/src/Algo_2024/inflearn/inf_02_대소문자변환.java
@@ -3,15 +3,22 @@ package Algo_2024.inflearn;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+/*
+    [ ASCII Code ]
+    총 25개의 문자
+    대문자 : 65 ~ 90
+    소문자 : 97 ~ 122
 
+    대문자와 소문자의 차이는 32의 차인
+ */
 public class inf_02_대소문자변환 {
     public static String solution(String str) {
         String answer = "";
         for (int i = 0; i < str.length(); i++) {
-            if (Character.isLowerCase(str.charAt(i))) {
-                answer += Character.toUpperCase(str.charAt(i));
+            if (str.charAt(i) >= 97 && str.charAt(i) <= 122) {
+                answer += (char) (str.charAt(i) - 32);
             } else {
-                answer += Character.toLowerCase(str.charAt(i));
+                answer += (char) (str.charAt(i) + 32);
             }
         }
         return answer;
