5f3a6e3d059454c72085f58d2f9a302b6d667bd3
Park <pbsu1213@hanmail.net>
2024-04-27 19:28:31 +0900

    feature: 특정 문자열 뒤집기 -> 버블정렬, Character.isAlphabetic();

---
+++ PS/src/Algo_2024/inflearn/inf_05_특정문자뒤집기.java
@@ -0,0 +1,40 @@
+package Algo_2024.inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+    lt -> left
+    rt -> right
+ */
+public class inf_05_특정문자뒤집기 {
+    public static String solution(String str) {
+        String answer = "";
+        char[] c = str.toCharArray();
+        int lt = 0;
+        int rt = str.length() - 1;
+
+        while (lt < rt) {
+            if (!Character.isAlphabetic(c[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(c[rt])) {
+                rt--;
+            } else {
+                char temp = c[lt];
+                c[lt] = c[rt];
+                c[rt] = temp;
+                lt++;
+                rt--;
+            }
+        }
+        return answer = String.valueOf(c);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+    }
+}
