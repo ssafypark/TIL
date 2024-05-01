59a29a8f4608ebd827271eef9eb515034e95494a
Park <pbsu1213@hanmail.net>
2024-05-01 19:24:58 +0900

    feature: int -> String, String.valueOf();

---
+++ PS/src/Algo_2024/inflearn/inf_11_문자열압축.java
@@ -0,0 +1,32 @@
+package Algo_2024.inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_11_문자열압축 {
+    public static String solution(String str) {
+        String answer = "";
+        str += " ";
+        int cnt = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                cnt++;
+            } else {
+                answer += str.charAt(i);
+                if (cnt > 1) {
+                    answer += String.valueOf(cnt);
+                    cnt = 1;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+    }
+}
