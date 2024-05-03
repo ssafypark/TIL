80080e14e43a21568a509cf3ba6e573f03140c68
Park <pbsu1213@hanmail.net>
2024-05-03 21:22:44 +0900

    feature: 피보나치 수열 -> void처리

--- PS/src/Algo_2024/inflearn/s2_array/inf_04_피보나치수열.java
+++ PS/src/Algo_2024/inflearn/s2_array/inf_04_피보나치수열.java
@@ -8,21 +8,22 @@ public class inf_04_피보나치수열 {
 
     private static int N;
 
-    public static int[] solution(int N) {
-        int[] answer = new int[N];
-        answer[0] = 1;
-        answer[1] = 1;
+    public static void solution(int N) {
+        int a = 1;
+        int b = 1;
+        int c;
+        System.out.print(a + " " + b + " ");
         for (int i = 2; i < N; i++) {
-            answer[i] = answer[i - 1] + answer[i - 2];
+            c = a + b;;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
         }
-        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        for (int x : solution(N)) {
-            System.out.print(x + " ");
-        }
+        solution(N);
     }
 }
