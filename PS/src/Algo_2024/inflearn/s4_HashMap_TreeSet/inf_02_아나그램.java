3df4f88219d90c5bd694fe8c3d39ad78b57a39ad
ssafypark <pbsu1213@hanmail.net>
2024-06-14 17:59:06 +0900

    feature: HashMap 문제, HashMap 관련 함수

---
+++ PS/src/Algo_2024/inflearn/s4_HashMap_TreeSet/inf_02_아나그램.java
@@ -0,0 +1,31 @@
+package Algo_2024.inflearn.s4_HashMap_TreeSet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class inf_02_아나그램 {
+
+    private static String str1;
+    private static String str2;
+
+    public static String solution(String str1, String str2) {
+        String answer = " ";
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (char x : str1.toCharArray()) {
+            hashMap.put(x, hashMap.getOrDefault(x, 0) + 1);
+        }
+        for (char x : str2.toCharArray()) {
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str1 = br.readLine();
+        str2 = br.readLine();
+        System.out.println(solution(str1, str2));
+    }
+}
