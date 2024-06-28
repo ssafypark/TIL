9c373829a330ac1753b1ab0f3b656a0a13d12feb
ssafypark <pbsu1213@hanmail.net>
2024-06-28 11:07:41 +0900

    feature: 재귀 피보나치 수열(배열 사용)

--- PS/src/Algo_2024/inflearn/s7_Recursive_Tree_Graph_DFS_BFS_기초/inf_04_피보나치재귀_메모이제이션.java
+++ PS/src/Algo_2024/inflearn/s7_Recursive_Tree_Graph_DFS_BFS_기초/inf_04_피보나치재귀_메모이제이션.java
@@ -9,20 +9,24 @@ package Algo_2024.inflearn.s7_Recursive_Tree_Graph_DFS_BFS_기초;
  **/
 public class inf_04_피보나치재귀_메모이제이션 {
 
+    private static int[] fibo;
+
     public static int DFS(int n) {
         if (n == 1) {
-            return 1;
+            return fibo[n] = 1;
         } else if (n == 2) {
-            return 1;
+            return fibo[n] = 1;
         } else {
-            return DFS(n - 2) + DFS(n - 1);
+            return fibo[n] = DFS(n - 2) + DFS(n - 1);
         }
     }
 
     public static void main(String[] args) {
         int n = 10;
+        fibo = new int[n + 1];
+        DFS(n);
         for (int i = 1; i <= n; i++) {
-            System.out.print(DFS(i) + " ");
+            System.out.print(fibo[i] + " ");
         }
     }
 }
