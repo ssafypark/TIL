5dbc11678bf9d21337675f6bca13f7b0a8287fb9
ssafypark <pbsu1213@hanmail.net>
2024-07-10 00:35:17 +0900

    feature: DFS로 부분집합 구하기

--- PS/src/Algo_2024/inflearn/s7_Recursive_Tree_Graph_DFS_BFS_기초/inf_06_부분집합구하기_DFS.java
+++ PS/src/Algo_2024/inflearn/s7_Recursive_Tree_Graph_DFS_BFS_기초/inf_06_부분집합구하기_DFS.java
@@ -16,7 +16,31 @@ package Algo_2024.inflearn.s7_Recursive_Tree_Graph_DFS_BFS_기초;
  **/
 public class inf_06_부분집합구하기_DFS {
 
-    public static void main(String[] args) {
+    private static int n;
+    private static int[] ch;
 
+    public static void DFS(int L) {
+        if (L == n + 1) { // 종착점에 왔을때
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) {
+                    tmp += (i + " ");
+                }
+            }
+            if (tmp.length() > 0) {
+                System.out.println(tmp);
+            }
+        } else {
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        n = 3;
+        ch = new int[n + 1];
+        DFS(1);
     }
 }
