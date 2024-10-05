3404caf059d7aa88e6255326caac69692a8cd982
ssafypark <pbsu1213@hanmail.net>
2024-10-05 20:17:41 +0900

    feature: DFS, Node 클래스 생성 및 전위, 중위, 후위 순회

--- PS/src/Algo_2024/inflearn/s7_Recursive_Tree_Graph_DFS_BFS_기초/inf_05_이진트리순회_DFS.java
+++ PS/src/Algo_2024/inflearn/s7_Recursive_Tree_Graph_DFS_BFS_기초/inf_05_이진트리순회_DFS.java
@@ -1,4 +1,49 @@
 package Algo_2024.inflearn.s7_Recursive_Tree_Graph_DFS_BFS_기초;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class inf_05_이진트리순회_DFS {
+
+    private static Node root;
+
+    public static void DFS(Node root) {
+        if (root == null) {
+            return;
+        } else {
+            // 전위순회
+            System.out.print(root.data + " ");
+            DFS(root.lt);
+            // 중위순회
+            // System.out.print(root.data + " ");
+            DFS(root.rt);
+            // 후위순회
+            // System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        DFS(root);
+    }
+}
+
+// 노트 클래스
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+    public Node(int val) {
+        data = val;
+        lt = null;
+        rt = null;
+    }
 }
