51e49a3a1229538c88f3d2f6518212c5b1785e41
ssafypark <pbsu1213@hanmail.net>
2024-10-15 21:19:05 +0900

    feature: 배열 정렬 후 앞뒤 배열 체크

---
+++ PS/src/Algo_2024/inflearn/s6_SortingAndSearching/inf_05_중복확인.java
@@ -0,0 +1,35 @@
+package Algo_2024.inflearn.s6_SortingAndSearching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class inf_05_중복확인 {
+
+    private static int N;
+    private static int[] arr;
+
+    public static String solution(int[] arr) {
+        String answer = "U";
+        Arrays.sort(arr);
+        for (int i = 0; i < N - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return "D";
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(arr));
+    }
+}
