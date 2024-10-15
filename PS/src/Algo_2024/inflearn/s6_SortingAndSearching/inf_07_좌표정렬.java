a8975a1d75cce2120ae3a9c8bc6a03c508092ca4
ssafypark <pbsu1213@hanmail.net>
2024-10-15 21:41:42 +0900

    feature: compare 사용, 복습

---
+++ PS/src/Algo_2024/inflearn/s6_SortingAndSearching/inf_07_좌표정렬.java
@@ -0,0 +1,38 @@
+package Algo_2024.inflearn.s6_SortingAndSearching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class inf_07_좌표정렬 {
+
+    private static int N;
+    private static int[][] arr;
+
+    public static int[][] solution(int[][] arr) {
+        Arrays.sort(arr, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
+            } else {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for (int[] x : solution(arr)) {
+            System.out.println(x[0] + " " + x[1]);
+        }
+    }
+}
