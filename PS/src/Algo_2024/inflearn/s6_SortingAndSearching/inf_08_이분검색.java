3403e745dfee44f260c08b5a3b56d34d0b95d695
ssafypark <pbsu1213@hanmail.net>
2024-10-17 19:55:42 +0900

    feature: 정렬

---
+++ PS/src/Algo_2024/inflearn/s6_SortingAndSearching/inf_08_이분검색.java
@@ -0,0 +1,39 @@
+package Algo_2024.inflearn.s6_SortingAndSearching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class inf_08_이분검색 {
+
+    private static int N;
+    private static int M;
+    private static int idx;
+    private static int[] arr;
+
+    public static int solution(int[] arr) {
+        int answer = -1;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length;i++) {
+            if (arr[i] == M) {
+                return i + 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(arr));
+    }
+}
