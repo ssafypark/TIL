7e281776607ac35bdfe42b5da6950d4b2ed5e900
ssafypark <pbsu1213@hanmail.net>
2024-10-30 19:58:29 +0900

    feature: 복습, implements Comparable, Override compareTo 사용

---
+++ PS/src/Algo_2024/inflearn/s6_SortingAndSearching/inf_07_좌표정렬.java
@@ -0,0 +1,56 @@
+package Algo_2024.inflearn.s6_SortingAndSearching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/*
+입력예제
+5
+2 7
+1 3
+1 2
+2 5
+3 6
+
+출력
+
+* */
+public class inf_07_좌표정렬 {
+
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        ArrayList<Point> list  = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        Collections.sort(list);
+        for (Point o : list) {
+            System.out.println(o.x + " " + o.y);
+        }
+    }
+}
+
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) {
+            return this.y - o.y;
+        } else {
+            return this.x - o.x;
+        }
+    }
+}
