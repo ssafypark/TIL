c6d2143d14aef1b885efc7e97d8bc51f3af8fc97
ssafypark <pbsu1213@hanmail.net>
2024-11-01 22:16:52 +0900

    feature: 복습, DFS, 8방향 탐색

---
+++ PS/src/Algo_2024/inflearn/s8_DFSBFS/inf_13_섬나라아일랜드_DFS.java
@@ -0,0 +1,48 @@
+package Algo_2024.inflearn.s8_DFSBFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class inf_13_섬나라아일랜드_DFS {
+
+    private static int N;
+    private static int answer;
+    private static int[][] board;
+    private static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    private static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+
+    public static void DFS(int x, int y) {
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N && board[nx][ny] == 1) {
+                board[nx][ny] = 0;
+                DFS(nx, ny);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 1) {
+                    answer++;
+                    board[i][j] = 0;
+                    DFS(i, j);
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
