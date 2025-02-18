d0f75f25606370ab187ed69962a744d60de2ead3
ssafypark <pbsu1213@hanmail.net>
2025-02-18 19:13:35 +0900

    feature: Comparable, compareTo 사용

---
+++ PS/src/Algo_2025/baekjoon/silver/BOJ_11650_좌표정렬.java
@@ -0,0 +1,44 @@
+package Algo_2025.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Point implements Comparable<Point> {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) {
+            return this.y - o.y;
+        }
+        return this.x - o.x;
+    }
+}
+
+public class BOJ_11650_좌표정렬 {
+
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        ArrayList<Point> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        Collections.sort(list);
+        for (Point p : list) {
+            System.out.println(p.x + " " + p.y);
+        }
+    }
+}
