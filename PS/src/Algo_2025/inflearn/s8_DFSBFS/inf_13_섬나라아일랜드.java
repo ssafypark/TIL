9024e5db7c986e58699dc579a1802f254959225c
ssafypark <pbsu1213@hanmail.net>
2025-02-18 18:59:09 +0900

    feature: DFS

---
+++ PS/src/Algo_2025/inflearn/s8_DFSBFS/inf_13_섬나라아일랜드.java
@@ -0,0 +1,47 @@
+package Algo_2025.inflearn.s8_DFSBFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class inf_13_섬나라아일랜드 {
+
+    private static int N;
+    private static int cnt;
+    private static int[][] map;
+    private static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    private static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+
+    public static void DFS(int x, int y) {
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N && map[nx][ny] == 1) {
+                map[nx][ny] = 0;
+                DFS(nx, ny);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1) {
+                    cnt++;
+                    DFS(i, j);
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
