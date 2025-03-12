0e8be7c77609b9edb36c9d23e9376af92b8a2a07
ssafypark <pbsu1213@hanmail.net>
2025-03-12 20:57:51 +0900

    feature: 정렬

---
+++ PS/src/Algo_2025/baekjoon/silver/BOJ_1015_수열정렬.java
@@ -0,0 +1,36 @@
+package Algo_2025.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1015_수열정렬 {
+
+    private static int N;
+    private static int[] A;
+    private static int[] B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        B = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = A[i];
+        }
+        Arrays.sort(B);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (A[i] == B[j]) {
+                    System.out.print(j + " ");
+                    B[j] = -1;
+                    break;
+                }
+            }
+        }
+    }
+}
